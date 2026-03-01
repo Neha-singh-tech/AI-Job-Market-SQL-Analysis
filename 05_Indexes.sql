6460996fb759adfb124533bc84b1ee60322686ea
Neha <neha.singh828765@gmail.com>
2026-03-01 14:05:47 +0530

    Create indexes for improved query performance
    Added performance optimization indexes for country and salary columns.

--- 05_Indexes.sql
+++ 05_Indexes.sql
@@ -1,3 +1,9 @@
+-- File: 05_Indexes.sql
+-- Project: AI Job Market SQL Analysis
+-- Description: Creating indexes to improve query
+-- performance on frequently used columns.
+
+------------------------Add performance optimization indexes---------------------------------------
 use Data_Analyst_Project
 select * from AI_job_market_cleaned
 
@@ -5,4 +11,5 @@ create Index Idx_country
 on AI_job_market_cleaned (country)
 
 create Index Idx_Salary
+
 on AI_job_market_cleaned (Salary_usd)
